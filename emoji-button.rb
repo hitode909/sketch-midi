9efb973ca137d363f93b9005943d88130f69dadb
hitode909 <hitode909@gmail.com>
2015-07-28 14:52:06 +0900

    日本語出せる

--- emoji-button.rb
+++ emoji-button.rb
@@ -2,6 +2,8 @@ require 'bundler'
 
 Bundler.require
 
+require 'shellwords'
+
 module AppleScript
   def self.stroke key
       system <<"EOF"
@@ -14,6 +16,10 @@ EOF
 
   def self.activate application
     system %Q|osascript -e 'activate application "#{ application }"'|
+  end
+
+  def self.paste text
+    system %Q(echo #{ Shellwords.escape text } | pbcopy && osascript -e 'tell application "System Events" to keystroke "v" using {command down}')
   end
 end
 
@@ -51,6 +57,10 @@ observer.on [151, 30, 127] { AppleScript.stroke ':shipit:' }
 observer.on [152, 30, 127] { AppleScript.stroke 'git fetch' }
 observer.on [184, 25, 0]   { AppleScript.stroke 'git pull' }
 observer.on [184, 25, 127] { AppleScript.stroke 'git push' }
+observer.on [152, 26, 127] { AppleScript.paste  '助かった' }
+observer.on [152, 27, 127] { AppleScript.paste  'じゃあそれで' }
+observer.on [152, 29, 127] { AppleScript.paste  '飲みに行くぞ' }
+
 observer.on [182, 23, 0]   { AppleScript.activate 'Google Chrome' }
 observer.on [182, 23, 32]  { AppleScript.activate 'Emacs' }
 observer.on [182, 23, 64]  { AppleScript.activate 'iTerm' }
